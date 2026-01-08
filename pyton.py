464112aac0f7b67a5a5120c92c0d80ece181d57c
fabriciofernandezalcala <fabricio.fernandez@ucb.edu.bo>
2026-01-08 12:25:41 -0400

    pyton

---
+++ pyton.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+x = 5
+y = 10
+resultado=x+y
+print("hola fabricio, el resultado es:")
+print(resultado)
+print("------------------------------------------------")
+print("calcular el area de rectamgulo")
+base=10.25
+altura=57.9
+area_rectangulo=base * altura
+print(area_rectangulo)
